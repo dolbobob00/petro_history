2ec5517e64e932ecc5aea36b09ca579873d0c29a
dolbobob01 <153915101+dolbobob01@users.noreply.github.com>
2024-09-18 19:19:31 +0600

    сщььшы

--- js/cursor.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('mousemove', (e) => {
-    const cursor = document.getElementById('neon-cursor');
-    cursor.style.left = `${e.clientX}px`;
-    cursor.style.top = `${e.clientY}px`;
-});
